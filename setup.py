259dc25568b91d0d2f0be8f14379216826ea9a26
Henry Roe <hroe@hroe.me>
2014-12-02 13:26:35 -0700

    forgot to increment version number to 0.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Py2MASS',
-      version='0.1.0',
+      version='0.1.2',
       description='Routines for accessing a self-hosted local copy of the 2MASS point-source and extended source catalogs (PSC, XSC)',
       long_description=open('README.md').read(),
       author='Henry Roe',
